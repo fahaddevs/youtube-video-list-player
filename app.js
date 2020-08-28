ccdb3f05c40473ec4928848d15bbdb67278623ac
fahaddevs <fahadbinfaiz1996@gmail.com>
2020-08-29 00:07:02 +0600

    add active action

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 
 $(document).ready(function(){
 
-  $( ".btn" ).each(function() {
+  $( ".list-btn" ).each(function() {
     $(this).on('click', function(){
       var dataSrc = $(this).attr("data-src");
       $('#video-wrapper').attr('src', dataSrc);
+
+      // add active class with "list-btn"
+      var element = $(this).parent("li");
+      if (element.hasClass("active")) {
+        element.find("li").removeClass("active");
+      }
+      else {
+        element.addClass("active");
+        element.siblings("li").removeClass("active");
+        element.siblings("li").find("li").removeClass("active");
+      }
+
     });
   });
-  
+
 });
